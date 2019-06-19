f75810311f3cffc3f7e20942f32c8bc69df3b7a0
ASUS <hamdi.jouini@esprit.tn>
2019-06-19 10:46:05 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	render() {
+		return(
+			<div className="mt-5">
+				<h2>Welcome to EventY.</h2>
+				<hr />
+			</div>
+		);
+	}
+}
+
+export default Home;
